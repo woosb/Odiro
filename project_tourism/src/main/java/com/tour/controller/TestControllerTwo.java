7b4e7f243662db6dac3b52e8f3d7bf18452832bc
woosb <woosb0115@nate.com>
2020-12-08 11:37:34 +0900

    TestControllerTwo

--- project_tourism/src/main/java/com/tour/controller/TestControllerTwo.java
+++ project_tourism/src/main/java/com/tour/controller/TestControllerTwo.java
@@ -2,6 +2,7 @@ package com.tour.controller;
 
 public class TestControllerTwo {
 	public TestControllerTwo() {
-		System.out.println("asdfasdfasdfasdf");
+		System.out.println("수정수정");
+		System.out.println("원래 있던것들은 pull 하면 어케되나");
 	}
 }
