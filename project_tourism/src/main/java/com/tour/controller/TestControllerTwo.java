63043f0defb614b917b0a4bccbd7f9a07db344ae
woosb <woosb0115@nate.com>
2020-12-08 11:35:23 +0900

    ads

--- project_tourism/src/main/java/com/tour/controller/TestControllerTwo.java
+++ project_tourism/src/main/java/com/tour/controller/TestControllerTwo.java
@@ -2,7 +2,6 @@ package com.tour.controller;
 
 public class TestControllerTwo {
 	public TestControllerTwo() {
-		System.out.println("깃 테스트");
-		System.out.println("테스트");
+		System.out.println("asdfasdfasdfasdf");
 	}
 }
